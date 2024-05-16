16d11feca8b66f9710aa37ee11a352cbe2eafbaa
Jason Newsom <jnewsom405@gmail.com>
2024-05-16 17:07:13 -0500

    Started day 1 of module 4! learned about React

---
+++ react_day_one/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import Body from './components/body';
+
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+        <Body />
+    </div>
+  );
+}
+
+export default App;
